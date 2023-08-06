9aed9377af5a388125b62e44e445674d3f2730b6
devarshn10 <93964347+devarshn10@users.noreply.github.com>
2023-08-06 19:10:55 +0530

    done

---
+++ server/models/Recipe.js
@@ -0,0 +1,35 @@
+// recipeModel.js
+
+const mongoose = require('mongoose');
+
+const recipeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: 'This is required.'
+    },
+    description: {
+        type: String,
+        required: 'This is required.'
+    },
+    email: {
+        type: String,
+        required: 'This is required.'
+    },
+    ingredients: {
+        type: Array,
+        required: 'This is required.'
+    },
+    category: {
+        type: String,
+        enum: ['Thai', 'American', 'Chinese', 'Mexican', 'Indian'],
+        required: 'This is required.'
+    },
+    image: {
+        type: String,
+        required: 'This is required.'
+    }
+});
+
+recipeSchema.index({name: 'text', description: 'text'});
+
+module.exports = mongoose.model('Recipe', recipeSchema);
